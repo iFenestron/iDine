ed7e8f3ed3c91ed967a3c3b93ba0c1c7ffa8ac30
iFenestron <kozhin.rm@gmail.com>
2021-01-20 16:52:04 +0300

    Bindings and forms

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,36 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Fenestron on 20.01.2021.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    
+    @State private var paymentType = 0
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0 ..< Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
