516e7a44da7489077733728da5fbeeae4bda73e0
iFenestron <kozhin.rm@gmail.com>
2021-01-20 11:48:40 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Fenestron on 20.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
