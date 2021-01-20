ca039a5d99fc6295fba93c27f8a5c6b48e535738
iFenestron <kozhin.rm@gmail.com>
2021-01-20 16:02:45 +0300

    Adding items to an order with @EnvironmentObject

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ItemDetail: View {
     var item: MenuItem
+    @EnvironmentObject var order: Order
     
     var body: some View {
         VStack {
@@ -23,6 +24,9 @@ struct ItemDetail: View {
             
             Text(item.description)
                 .padding()
+            Button("Order This") {
+                self.order.add(item: self.item)
+            }.font(.headline)
             Spacer()
         }
         .navigationBarTitle(item.name, displayMode: .inline)
@@ -30,9 +34,11 @@ struct ItemDetail: View {
 }
 
 struct ItemDetail_Previews: PreviewProvider {
+    static let order = Order()
+    
     static var previews: some View {
         NavigationView {
-                    ItemDetail(item: MenuItem.example)
+            ItemDetail(item: MenuItem.example).environmentObject(order)
                 }
     }
 }
