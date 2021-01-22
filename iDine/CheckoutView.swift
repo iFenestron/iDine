0f5884530df87d4e0898795d14d16a879497f307
iFenestron <kozhin.rm@gmail.com>
2021-01-22 07:51:53 +0300

    Presenting an alert

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -16,6 +16,7 @@ struct CheckoutView: View {
     @State private var loyaltyNumber = ""
     @State private var paymentType = 0
     @State private var tipAmount = 1
+    @State private var showingPaymentAlert = false
     
     var totalPrice: Double {
         let total = Double(order.total)
@@ -50,12 +51,14 @@ struct CheckoutView: View {
                 Text("TOTAL: $\(totalPrice, specifier: "%.2f")")
             ) {
                 Button("Confirm order") {
-                    // place the order
+                    self.showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationBarTitle(Text("Payment"), displayMode: .inline)
-    }
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was $\(totalPrice, specifier: "%.2f") – thank you!"), dismissButton: .default(Text("OK")))
+        }    }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
